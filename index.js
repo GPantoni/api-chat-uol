922d4841104a4510ba8e386900e442f56db7a933
Glauco Pantoni <glaucopantoni@gmail.com>
2022-01-29 17:14:41 -0300

    Add the mongoConnect()

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import express, { json } from "express";
 import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
+import joi from "joi";
+import dayjs from "dayjs";
+import { stripHtml } from "string-strip-html";
 import dotenv from "dotenv";
 dotenv.config();
 
 const server = express();
-server.use(json);
-server.use(cors);
+server.use(json());
+server.use(cors());
+
+async function mongoConnect() {
+  const mongoClient = new MongoClient(process.env.MONGO_URI);
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("chatBase");
+    return { mongoClient, db };
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 server.post("/participants", (req, res) => {});
 
